c43984810d98749176123354c519156edd07adfc
sythenA <sythena@gmail.com>
2018-08-15 23:14:45 +0800

    20180815 23:15 Build
    # Can load TEC file as node layer and set to vector plot theme.

---
+++ vectorPlot/vectorDiag.py
@@ -0,0 +1,155 @@
+
+import os
+from qgis.core import QgsMapLayerRegistry, QgsMarkerSymbolV2
+from qgis.core import QgsSimpleMarkerSymbolLayerV2, QgsDataDefined, QgsSymbolV2
+from qgis.core import QgsVectorFieldSymbolLayer, QgsSingleSymbolRendererV2
+from qgis.PyQt import QtGui, uic
+from qgis.PyQt.QtGui import QFileDialog, QListWidgetItem, QColor, QMenu
+from qgis.PyQt.QtCore import QSettings
+from ..tools.TECfile import TECfile
+from random import randint
+import subprocess
+
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'vectorPlot.ui'))
+
+
+class vecPlotDialog(QtGui.QDialog, FORM_CLASS):
+    def __init__(self, iface, parent=None):
+        super(vecPlotDialog, self).__init__(parent)
+        self.setupUi(self)
+
+        self.iface = iface
+        self.settings = QSettings('ManySplendid', 'SRH2D_TEC_Viewer')
+
+        self.addTecBtn.clicked.connect(self.addTecFiles)
+        self.deleteTecBtn.clicked.connect(self.deleteTecFile)
+
+    def addTecFiles(self):
+        simFolder = os.path.join(self.settings.value('projFolder'), 'sim')
+        TecFiles = QFileDialog().getOpenFileNames(directory=simFolder)
+        if TecFiles:
+            for file in TecFiles:
+                item = TECfile(self.tecList, QListWidgetItem.Type, file,
+                               self.iface)
+                item.readTEC(file)
+                item.readVariables(item.dat)
+                self.tecList.addItem(item)
+                self.addAttribute()
+
+    def deleteTecFile(self):
+        self.tecList.takeItem(self.tecList.currentRow())
+
+    def addAttribute(self):
+        item = self.tecList.item(0)
+        _attr = item.attributes
+        attributes = list()
+        for item in _attr:
+            attributes.append(item[0])
+
+        if self.xAttrCombo.count() == 0:
+            self.xAttrCombo.addItems(attributes)
+            self.yAttrCombo.addItems(attributes)
+
+
+class vecPlot:
+    def __init__(self, iface):
+        self.iface = iface
+        self.settings = QSettings('MantSplendid', 'SRH2D_TEC_Viewer')
+        self.registry = QgsMapLayerRegistry.instance()
+
+        self.dlg = vecPlotDialog(self.iface)
+        self.dlg.tecList.customContextMenuRequested.connect(
+            self.rightClickOnItem)
+
+    def run(self):
+        self.dlg.show()
+        result = self.dlg.exec_()
+
+        defaultFolder = self.defaultFolder()
+        if result:
+            self.xAttr = self.dlg.xAttrCombo.currentText()[0:10]
+            self.yAttr = self.dlg.yAttrCombo.currentText()[0:10]
+
+            self.setOutput(defaultFolder)
+
+    def defaultFolder(self):
+        projFolder = self.settings.value('projFolder')
+
+        if projFolder:
+            outDir = os.path.join(projFolder, 'FlowDirection')
+
+            if os.path.isdir(self.outDir):
+                subprocess.call(['cmd', '/c', 'RD', '/S', '/Q', outDir])
+                subprocess.call(['cmd', '/c', 'mkdir', outDir])
+            else:
+                subprocess.call(['cmd', '/c', 'mkdir', outDir])
+
+            return outDir
+        else:
+            return ''
+
+    def rightClickOnItem(self, pos):
+        cursorPos = self.dlg.tecList.mapToGlobal(pos)
+        subMenu = QMenu()
+        subMenu.addAction('Set Out Folder', self.setItemFolder)
+        # subMenu.addAction('Set XY Attribute', self.exportTEC)
+
+        subMenu.exec_(cursorPos)
+
+    def setOutput(self, presetFolder):
+        outputFolder = QFileDialog().getExistingDirectory(
+            directory=presetFolder)
+        for i in range(0, self.dlg.tecList.count()):
+            item = self.dlg.tecList.item(i)
+            if not item.outDir:
+                item.outDir = outputFolder
+            item.genNodeLayer()
+            vlayer = self.registry.addMapLayer(item.nodeLayer)
+            if item.xAttr:
+                xAttr = item.xAttr
+            else:
+                xAttr = self.xAttr
+            if item.yAttr:
+                yAttr = item.yAttr
+            else:
+                yAttr = self.yAttr
+
+            renderer = self.buildRenderer(xAttr, yAttr)
+            vlayer.setRendererV2(renderer)
+
+    def setItemFolder(self):
+        item = self.dlg.tecList.currentItem()
+        fileFolder = os.path.dirname(item.filePath)
+        outputFolder = QFileDialog().getExistingDirectory(
+            directory=fileFolder)
+        if outputFolder:
+            item.outDir = outputFolder
+
+    def buildRenderer(self, xAttr, yAttr):
+        color = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+        symbol = QgsMarkerSymbolV2.createSimple({})
+        symbol.deleteSymbolLayer(0)
+
+        prop = QgsDataDefined()
+        prop.setField("size")
+        prop.setActive(True)
+
+        symbol_layer = QgsSimpleMarkerSymbolLayerV2()
+        symbol_layer.setColor(color)
+        symbol_layer.setOutputUnit(QgsSymbolV2.MapUnit)
+        symbol_layer.setDataDefinedProperty("size", prop)
+        symbol.appendSymbolLayer(symbol_layer)
+
+        symbol_layer = QgsVectorFieldSymbolLayer()
+        symbol_layer.setColor(color)
+        symbol_layer.VectorFieldType(QgsVectorFieldSymbolLayer.Cartesian)
+        symbol_layer.AngleUnits(QgsVectorFieldSymbolLayer.Degrees)
+        symbol_layer.setXAttribute(xAttr)
+        symbol_layer.setYAttribute(yAttr)
+        symbol.appendSymbolLayer(symbol_layer)
+
+        renderer = QgsSingleSymbolRendererV2(symbol)
+
+        return renderer
